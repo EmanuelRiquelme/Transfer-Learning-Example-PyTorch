cd53f88ccbb51a3270c9ac921244b2b9aaeb3fff
EmanuelRiquelme <112744690+EmanuelRiquelme@users.noreply.github.com>
2022-12-22 23:46:14 -0300

    Add files via upload

---
+++ model.py
@@ -0,0 +1,28 @@
+import torch
+import torchvision
+import torch.nn as nn
+from torchvision.models import resnet34, ResNet34_Weights
+
+def load_model(model):
+    model.load_state_dict(torch.load(f'{os.getcwd()}/resnet.pt'))
+def save_model(model):
+    torch.save(model.state_dict(), f'{os.getcwd()}/resnet.pt')
+
+class Model(nn.Module):
+    def __init__(self,num_classes = 10):
+        super(Model, self).__init__()
+        self.num_classes = num_classes
+        self.softmax = nn.Softmax(-1)
+        self.model = self.__load_model__()
+
+    def __load_model__(self):
+        model = resnet34(weights=ResNet34_Weights.IMAGENET1K_V1)
+        for param in model.parameters():
+            param.requires_grad = False
+        model.fc = nn.Linear(512,self.num_classes)
+        return model
+
+    def forward(self,input_data):
+        input_data =  self.model(input_data)
+        return self.softmax(input_data)
+
